c7677fdbe959427cdb318739f5e016b78a0c3e64
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2024-11-15 17:09:35 -0300

    test: change file to raise lint error

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <Image
+      className={styles.logo}
+      src="/next.svg"
+      alt="Next.js logo"
+      width={180}
+      height={38}
+      priority
+    />
         <ol>
           <li>
             Get started by editing <code>src/app/page.js</code>.
